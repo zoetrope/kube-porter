3e4bf324e462146792f3f9fa2c6d6fc3feb0b7d1
zoetrope <a.ikezoe@gmail.com>
2023-01-11 22:10:52 +0900

    Add list subcommand

--- cmd/logfile.go
+++ cmd/logfile.go
@@ -14,7 +14,10 @@ var logfileCmd = &cobra.Command{
 	Short: "Print the name of log file for declarative-port-forwarder",
 	Long: `Print the name of log file for declarative-port-forwarder
 You can view log by passing the result of this command as follows:
-> tail -f $(declarative-port-forwarder logfile)
+
+$ cat $(declarative-port-forwarder logfile)
+
+$ tail -f $(declarative-port-forwarder logfile)
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		c := pkg.NewClient(rootOpts.socket)
@@ -23,21 +26,16 @@ You can view log by passing the result of this command as follows:
 			fmt.Fprintln(os.Stderr, "declarative-port-forwarder is not yet running")
 			return err
 		}
-		c.Get("/logfile")
+		logfile, err := c.Get("/logfile")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get logfile: %v\n", err)
+			return err
+		}
+		fmt.Fprintf(os.Stdout, logfile)
 		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(logfileCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// logfileCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// logfileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
